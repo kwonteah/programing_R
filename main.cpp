40b63ce9a589199a8cbb9e646e49eee2a7ffd902
taehyuk kwon <tyk6974@gmail.com>
2023-03-27 19:21:06 +0900

    first commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+int main()
+{
+	while(1)
+	{
+		std::cout<<"*";
+		
+		if(kbhit()
+		{
+			sid::cout<<"snffla";
+			if(getch()==27)
+				break;
+		}
+	}
+	return 0;
+}
